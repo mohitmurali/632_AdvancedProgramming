0f61b6011c78f1d22068449606e9c6e420e677a2
mohitmurali <mmurali35371@ucumberlands.edu>
2025-03-14 07:53:35 -0400

    Create assignment2_section2.js

---
+++ assignment2_section2.js
@@ -0,0 +1,20 @@
+// Scoping & Closures
+function funcCounter() {
+    let count = 0;  // Create a local variable
+    
+    function addIncrement() {
+        count += 1;  // Allows to use outer variable
+        return count;
+    }
+
+    return addIncrement;
+}
+
+// Call the function & Create independent counters
+const counter1 = funcCounter();
+const counter2 = funcCounter();
+
+console.log("Counter 1:", counter1());  // Output: 1
+console.log("Counter 1:", counter1());  // Output: 2
+console.log("Counter 2:", counter2());  // Output: 1
+console.log("Counter 2:", counter2());  // Output: 2
