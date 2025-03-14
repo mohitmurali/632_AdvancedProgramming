e4e7aa26940fcb4673e5dd7618d23693c33bff60
mohitmurali <mmurali35371@ucumberlands.edu>
2025-03-14 17:51:11 -0400

    Part 2: Memory Management code in Rust language

--- assignment2_section3.rs
+++ assignment2_section3.rs
@@ -1,5 +1,9 @@
 // Program to multiple numbers
+
+use std::time::Instant;
 fn main() {
+
+    let start = Instant::now();
     // Allocating memory inside array
     let num = vec![2,5,10]; 
 
@@ -8,5 +12,8 @@ fn main() {
 
     // Result
     println!("Product of numbers in Rust: {}", multiply);
+    
+    let duration = start.elapsed();
+    println!("Execution Time: {:?}", duration);
 
 }
