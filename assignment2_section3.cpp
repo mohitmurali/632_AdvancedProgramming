9212e09e9c32345a48d4547261d7a3939902729e
mohitmurali <mmurali35371@ucumberlands.edu>
2025-03-14 10:34:58 -0400

    Create assignment2_section3.cpp

---
+++ assignment2_section3.cpp
@@ -0,0 +1,22 @@
+// Program to multiply numbers
+import java.util.ArrayList;
+
+public class mulitplyCalc {
+    public static void main(String[] args) 
+    {
+        // Allocate memory inside array
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(2);
+        num.add(5);
+        num.add(10);
+
+        // Multiply calculator
+        int pdt = 1;
+        for (int i = 0; i < num.size(); i++) {
+            pdt *= num.get(i);
+        }
+
+        // Result
+        System.out.println("Product of numbers in Java: " + pdt);
+    }
+}
